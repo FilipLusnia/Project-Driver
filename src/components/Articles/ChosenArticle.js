063baba87fc68ca7a7f437c06b278d63296f0aa8
Filip Luśnia <uszaty223@onet.pl>
2020-08-16 18:01:50 +0200

    selected article styling

--- src/components/Articles/ChosenArticle.js
+++ src/components/Articles/ChosenArticle.js
@@ -9,6 +9,8 @@ import Loader from 'react-loader-spinner';
 
 
 function ChosenArticle(props) {
+
+  window.scrollTo(0, 0);
   
   const {articles} = props;
   const articleName = props.match.params.articlename;
@@ -20,10 +22,13 @@ function ChosenArticle(props) {
       <div>
         {
         currentArticle ?
-          <p>{currentArticle.title}</p>
-          
+          <div className="chosen_article_container">
+            <h1 className="chosen_article_title">{currentArticle.title}</h1>
+            <h2 className="chosen_article_thumbtext">{currentArticle.thumbText}</h2>
+            <img src={currentArticle.img} alt="cover" className="chosen_article_img"></img>
+            <h className="chosen_article_text">{currentArticle.text}</h>
+          </div>
         :
-
           <div className="article_list_loader">
             <Loader type="Circles" color="#5365ff" height={80}/>
           </div>
