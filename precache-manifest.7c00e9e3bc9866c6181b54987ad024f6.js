7cbdf23cbb1390c95d28a3e528014e25ee0b4118
Filip Luśnia <uszaty223@onet.pl>
2020-09-04 09:26:54 +0200

    Updates

---
+++ precache-manifest.7c00e9e3bc9866c6181b54987ad024f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443c2fe8c62812aa315510ceaa637873",
+    "url": "/Project-Driver/index.html"
+  },
+  {
+    "revision": "80790081a507fc927da0",
+    "url": "/Project-Driver/static/css/main.78d2566c.chunk.css"
+  },
+  {
+    "revision": "dd3f1cc8f9eac7740efa",
+    "url": "/Project-Driver/static/js/2.61eab971.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Project-Driver/static/js/2.61eab971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80790081a507fc927da0",
+    "url": "/Project-Driver/static/js/main.2d3d4e35.chunk.js"
+  },
+  {
+    "revision": "c185cad1c436def78b7c",
+    "url": "/Project-Driver/static/js/runtime-main.a850686e.js"
+  },
+  {
+    "revision": "055932a0e3bad5ed07370fe80939cd59",
+    "url": "/Project-Driver/static/media/PD-logo.055932a0.svg"
+  },
+  {
+    "revision": "3cea27a770b8d5b6047498f26aa56213",
+    "url": "/Project-Driver/static/media/login-background.3cea27a7.jpg"
+  },
+  {
+    "revision": "97020ee1b14f9bc65c7c6f3be20f3abf",
+    "url": "/Project-Driver/static/media/main-background.97020ee1.jpeg"
+  },
+  {
+    "revision": "42e0cd4030074eea6dca29393dea27d5",
+    "url": "/Project-Driver/static/media/register-background.42e0cd40.jpg"
+  },
+  {
+    "revision": "3c03080ec93f69b32a462265610614f9",
+    "url": "/Project-Driver/static/media/user-icon.3c03080e.png"
+  }
+]);
